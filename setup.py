5ebc606c8b0fde1b9730a4d796368918f7700c15
zencore <34684454+appstore-zencore@users.noreply.github.com>
2018-04-02 09:00:51 +0800

    add python 2.x support

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+from io import open
 from setuptools import setup
 from setuptools import find_packages
 
@@ -12,7 +13,7 @@ requires = [
 
 setup(
     name="dictop",
-    version="0.1.1",
+    version="0.1.2",
     description="DICT-OPERATION allow you select data value from a dict-instance with dot separated path, and update.",
     long_description=long_description,
     url="https://github.com/appstore-zencore/dictop",
